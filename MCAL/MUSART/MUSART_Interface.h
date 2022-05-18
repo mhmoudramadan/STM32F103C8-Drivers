7b625d74e4ffdc253fbd962751fb9e2737e162de
mhmoudramadan <mhmoudramadan111@gmail.com>
2022-05-19 00:34:01 +0200

    1:0:0

---
+++ MCAL/MUSART/MUSART_Interface.h
@@ -0,0 +1,28 @@
+/*
+ * MUART_Interface.h
+ *
+ *  Created on: May 14, 2022 1:06:07 AM 
+ *  Topic-->UART_DRIVER_PRO
+ *      Author: mahmoud ramadan
+ */
+
+#ifndef MCAL_MUSART_MUSART_INTERFACE_H_
+#define MCAL_MUSART_MUSART_INTERFACE_H_
+
+/*UART Channel*/
+typedef enum
+{
+	UART1,
+	UART2,
+	UART3,
+	UART4,
+	UART5
+}MUART_Channel;
+/*ProtoTypes*/
+void MUART_VidInit(MUART_Channel copy_Channel);
+void MUART_VidTransmit_Data_Synchronous(MUART_Channel copy_Channel , uint8 *Puint8_data);
+uint8 MUART_VidReceive_Data_Synchronous(MUART_Channel copy_Channel , uint8 *Puint8_data);
+uint8 MUART_VidReceive_Byte_Synchronous(MUART_Channel copy_Channel);
+void MUART_VidReceive_Data_Asynchronous(MUART_Channel copy_Channel ,void (*PtrToFun)(uint8));
+
+#endif /* MCAL_MUSART_MUSART_INTERFACE_H_ */
